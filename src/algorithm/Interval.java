aa5e561f9ef4b244139e712c74608a297450fe29
Spencer-Lion <3252468505@qq.com>
2022-04-03 16:28:30 +0800

    初次提交已完成的算法

---
+++ src/algorithm/Interval.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+/**
+ * Definition for an interval
+ */
+public class Interval {
+    int start;
+    int end;
+
+    Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+}
---
+++ src/algorithm/ListNode.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+/**
+ * Definition for a binary Listnode
+ */
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+
+
+
