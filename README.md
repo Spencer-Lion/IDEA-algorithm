c4b31e3126544813b82b2b0e2e082f5828d44569
Spencer-Lion <3252468505@qq.com>
2022-04-17 15:15:54 +0800

    完善知识点，main类作为调试用

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # IDEA-algorithm
+
 数据结构和算法，在IDEA中实现
+
+需要复习的知识点： java中treemap和hashmap的区别是什么？ 
+1、HashMap中元素是没有顺序的；TreeMap中所有元素都是有某一固定顺序的。
+2、HashMap继承AbstractMap类，是基于hash表实现的；TreeMap继承SortedMap类，是基于红黑树实现的。
+
+重写比较器
+    // 方法三: 使用list并自己实现Comparator, 比较能体现算法的思路
+    public static void withComparator() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<String> list = new ArrayList<>();
+        String s = "";
+        while ((s = br.readLine()) != null) {
+            list.add(s);
+        }
+        br.close();
+ 
+        list.sort(new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                int i = 0;
+                while (i < s1.length() && i < s2.length()) {
+                    if (s1.charAt(i) != s2.charAt(i)) {
+                        return (s1.charAt(i) > s2.charAt(i))? 1: -1;
+                    }
+                    i++;
+                }
+                if (s1.length() == s2.length()) {
+                    return 0;
+                } else {
+                    return (s1.length() > s2.length())? 1: -1;
+                }
+            }
+        });
+ 
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+HJ35 蛇形矩阵
+HJ54 表达式求值
--- src/algorithm/newcode/Main.java
+++ src/algorithm/newcode/Main.java
@@ -1,75 +1,10 @@
 package algorithm.newcode;
 
-import java.util.*;
 import java.util.Scanner;
 
 public class Main {
 
-    /**
-     * HJ101 输入整型数组和排序标识，对其元素按照升序或降序进行排序
-     */
     public static void main(String[] args) {
-        Main solution = new Main();
-        Scanner in = new Scanner(System.in);
-        while (in.hasNextInt()) {
-            int n = in.nextInt();
-            Integer[] array = new Integer[n];
-            for (int i = 0; i < n; i++) {
-                array[i] = in.nextInt();
-            }
-            int flag = in.nextInt();
-            solution.sortIntegerArray(array, flag);
-            solution.output(array);
-        }
-
-    }
-
-    public void sortIntegerArray(Integer[] pIntegerArray, int iSortFlag) {
-        // 按升序
-        if (iSortFlag == 0) {
-            Arrays.sort(pIntegerArray, new Comparator<Integer>() {
-                public int compare(Integer o1, Integer o2) {
-                    return o1 - o2;
-                }
-            });
-        }
-        // 按降序
-        else {
-            Arrays.sort(pIntegerArray, new Comparator<Integer>() {
-                public int compare(Integer o1, Integer o2) {
-                    return o2 - o1;
-                }
-            });
-        }
-    }
-
-    public void output(Integer[] array) {
-        for (Integer i : array) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-    }
 
 
-    /**
-     * 返回去重后的数字的逆序
-     */
-    public void newInt() {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        char[] chars = s.toCharArray();
-        //Set中的LinkedHashSet就是有序的
-        LinkedHashSet<Character> set = new LinkedHashSet<>();
-        for (int i = chars.length - 1; i >= 0; i--) {
-            set.add(chars[i]);
-        }
-//        s = "";
-        StringBuilder sbr = new StringBuilder();
-        Iterator<Character> it = set.iterator();
-        while (it.hasNext()) {
-//            s += it.next();
-            sbr.append(it.next());
-        }
-        System.out.println(sbr.toString());
-    }
 }
